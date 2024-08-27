f93d2e2c5cd77cf82e9e3819590c50aee3d654ee
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-08-27 21:06:12 +0200

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+comprises an algorithm to calculate
+the minimum number of operations to get n 'H' chars given n.
+"""
+def minOperations(n):
+    """
+    Calculates the minimum number of operations
+    to get n 'H' characters.
+    Args:
+        n (int) - The target number of 'H' characters.
+    Returns:
+        int: The minimum number of operations required, or 0 if impossible.
+
+    """
+    n <= 1:
+        return 0
+    operations = 0
+    factors = 2
+    while n > 1:
+        while n % factors == 0:
+            operations += factors
+            n //= factors
+        factors += 1
+    return operations
+
