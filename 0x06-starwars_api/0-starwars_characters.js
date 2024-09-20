d6e1a29ccbda61229a826a7ac4f15db17dc59b69
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-20 05:27:25 +0200

    used request-promise-native to check why the checker was failing

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // fecth all characters of Star Wars movie.
 
-const request = require('request');
-const util = require('util');
-const rp = util.promisify(request);
+const rp = require('request-promise-native');
 
 if (process.argv.length < 3) {
   process.exit(1);
@@ -14,12 +12,12 @@ const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 async function fetchCharacter (url) {
   const resp = await rp(url);
-  return JSON.parse(resp.body).name;
+  return JSON.parse(resp).name;
 }
 async function starWarMovieCharacters () {
   try {
     const movieResp = await rp(movieUrl);
-    const movie = JSON.parse(movieResp.body);
+    const movie = JSON.parse(movieResp);
     const characterUrls = movie.characters;
     for (const url of characterUrls) {
       const name = await fetchCharacter(url);
