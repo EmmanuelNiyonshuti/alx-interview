dec52aef01e2c95551f4fde47ff4d290355666a9
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-22 10:46:35 +0200

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,33 @@
 #!/usr/bin/node
-/**
- * Star War API
- */
-/*
-Promisfy request
-*/
+// fecth all characters of Star Wars movie.
+
 const request = require('request');
 const util = require('util');
-const requestPromise = util.promisify(request);
+const rp = util.promisify(request);
 
 if (process.argv.length < 3) {
   process.exit(1);
 }
-const actorId = process.argv[2];
-/**
- * getCharacter - print list of actor
- * @param {string} id
- */
-async function getCharacter (id) {
-  const fullPath = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+const movieId = process.argv[2];
+const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+async function fetchCharacter (url) {
+  const resp = await rp(url);
+  return JSON.parse(resp.body).name;
+}
+async function starWarMovieCharacters () {
   try {
-    const response = await requestPromise({ uri: fullPath });
-    const data = await JSON.parse(response.body);
-    const characters = data.characters;
-    const listOfNames = [];
-    for (let i = 0; i < characters.length; i++) {
-      try {
-        const response = await requestPromise(characters[i]);
-        const data = await JSON.parse(response.body);
-        listOfNames.push(await data.name);
-      } catch (error) {
-        console.error(error);
-      }
+    const movieResp = await rp(movieUrl);
+    const movie = JSON.parse(movieResp.body);
+    const characterUrls = movie.characters;
+    for (const url of characterUrls) {
+      const name = await fetchCharacter(url);
+      console.log(name);
     }
-    for (const actor of listOfNames) {
-      console.log(actor);
-    }
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err.message);
   }
 }
-/* call getCharacter on argv */
-getCharacter(actorId);
+
+starWarMovieCharacters();
