17e6f4f8782b5af78c9068f9920784adb7d82fd9
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-06 09:41:33 +0200

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Comprises coin change problem solution.
+coin change problem solution with greedy approach.
 """
 
 
@@ -19,13 +19,12 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            count += 1
+        if total == 0:
+            return count
+    return -1
