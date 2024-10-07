139ab94b9aba5d1cd5515a03e6316bbda48cb7b3
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-10-07 13:09:16 +0200

    initial commit

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+comprises a function that finds the perimeter of an island.
+'''
+from typing import List
+
+
+def island_perimeter(grid: List[int]):
+    '''
+    finds island perimeter.
+    Args:
+        grid (list) - nested list representing a grid.
+    Return:
+        int - island perimeter.
+    '''
+    if not grid:
+        return
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r-1][c] == 1:
+                    perimeter -= 1
+                if r < rows - 1 and grid[r+1][c] == 1:
+                    perimeter -= 1
+                if c > 0 and grid[r][c-1] == 1:
+                    perimeter -= 1
+                if c < cols-1 and grid[r][c+1] == 1:
+                    perimeter -= 1
+    return perimeter
